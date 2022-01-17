25da0577b4dcaee8f7aac493e42bf5fcc620ace2
Ababu Alemu <63737795+ababuu@users.noreply.github.com>
2022-01-17 11:21:47 +0300

    Update InsertionSort1.js

--- InsertionSort1.js
+++ InsertionSort1.js
@@ -1,1 +1,10 @@
-
+function insertionSort1(n, arr) {
+    // Write your code here
+    let element = arr[n-1];
+    for(var i=n-2;i>=0 && arr[i]>element;i--){
+        arr[i+1]=arr[i];
+        console.log(arr.join(' '));
+    }
+    arr[i+1]=element;
+    console.log(arr.join(' '));
+}
